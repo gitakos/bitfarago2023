367c967449422d6278d50cf967f693ce9e49d730
KrauszMartonAlajos <krauszmarcika@gmail.com>
2023-09-17 16:29:31 +0200

    megölöm magam

--- 1. forduló/Aloldalak/bemutato/script.js
+++ 1. forduló/Aloldalak/bemutato/script.js
@@ -16,10 +16,56 @@ function Ora() {
 function DatumAtalakitoCsoda(nap,ev,honap,nap_datum){
     var honapok = ["Január","Február","Március","Április","Május","Június","Július","Augusztus","Szeptember","Október","November","December"];
     var napok = ["Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap"];
-    var margyar_datum = napok[nap-1]+", "+ev+". "+honapok[honap]+" "+nap_datum+".";
+    var margyar_datum = nap+", "+ev+". "+honapok[honap]+" "+nap_datum+".";
     return margyar_datum;
 }
 
+function toggleFullText(tldrId, fullTextId, buttonId) {
+    var tldr = document.getElementById(tldrId);
+    var fullText = document.getElementById(fullTextId);
+    var button = document.getElementById(buttonId);
+    tldr.style.overflow="hidden";
+    fullText.style.overflow="hidden";
+    tldr.style.height = tldr.scrollHeight + "px";
+    fullText.style.height = fullText.scrollHeight + "px";
+    tldr.style.transition = "opacity 0.5s ease, height 0.5s ease";
+    fullText.style.transition = "opacity 0.5s ease, height 0.5s ease";
+
+
+
+
+    if (tldr.style.display === "none") {
+
+      tldr.style.opacity = "0";
+      tldr.style.height = "0";
+      fullText.style.opacity = "0";
+      fullText.style.height = "0";
+      button.innerHTML = "Mutass többet";
+      setTimeout(function() {
+        tldr.style.display = "block";
+        fullText.style.display = "none";
+        setTimeout(function() {
+          tldr.style.opacity = "1";
+          tldr.style.height = tldr.scrollHeight + "px";
+        }, 0);
+      }, 500);
+    } else {
+
+      fullText.style.opacity = "0";
+      fullText.style.height = "0";
+      tldr.style.opacity = "0";
+      tldr.style.height = "0";
+      button.innerHTML = "Kevesebb";
+      setTimeout(function() {
+        fullText.style.display = "block";
+        tldr.style.display = "none";
+        setTimeout(function() {
+          fullText.style.opacity = "1";
+          fullText.style.height = fullText.scrollHeight + "px";
+        }, 0);
+      }, 500);
+    }
+  }
 
 
 setInterval(Ora, 1000);
