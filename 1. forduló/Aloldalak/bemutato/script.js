16511157131908266cd9096939c7a48eaf49fb07
Kovács Ákos <kovacs.akos@ckik.loc>
2023-09-25 12:18:32 +0200

    bemutato

--- 1. forduló/Aloldalak/bemutato/script.js
+++ 1. forduló/Aloldalak/bemutato/script.js
@@ -1,4 +1,4 @@
-function Ora() {
+/*function Ora() {
     var akt_ido = new Date();
     var datum = document.getElementById('datum');
     var ev = akt_ido.getFullYear();
@@ -69,4 +69,4 @@ function toggleFullText(tldrId, fullTextId, buttonId) {
 
 
 setInterval(Ora, 1000);
-Ora();
+Ora();*/
