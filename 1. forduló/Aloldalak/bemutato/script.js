6f7298f60057aa23ce2334acdf9a0214f30a2973
bUMmrR <agocsdaniel5454@gmail.com>
2023-09-27 15:19:50 +0200

    A képeink fixxelése

--- 1. forduló/Aloldalak/bemutato/script.js
+++ 1. forduló/Aloldalak/bemutato/script.js
@@ -21,52 +21,51 @@ function DatumAtalakitoCsoda(nap,ev,honap,nap_datum){
 }
 
 function toggleFullText(tldrId, fullTextId, buttonId) {
-    var tldr = document.getElementById(tldrId);
-    var fullText = document.getElementById(fullTextId);
-    var button = document.getElementById(buttonId);
-    tldr.style.overflow="hidden";
-    fullText.style.overflow="hidden";
-    tldr.style.height = tldr.scrollHeight + "px";
-    fullText.style.height = fullText.scrollHeight + "px";
-    tldr.style.transition = "opacity 0.5s ease, height 0.5s ease";
-    fullText.style.transition = "opacity 0.5s ease, height 0.5s ease";
+  var tldr = document.getElementById(tldrId);
+  var fullText = document.getElementById(fullTextId);
+  var button = document.getElementById(buttonId);
+  tldr.style.overflow="hidden";
+  fullText.style.overflow="hidden";
+  tldr.style.height = tldr.scrollHeight + "px";
+  fullText.style.height = fullText.scrollHeight + "px";
+  tldr.style.transition = "opacity 0.5s ease, height 0.5s ease";
+  fullText.style.transition = "opacity 0.5s ease, height 0.5s ease";
 
 
 
 
-    if (tldr.style.display === "none") {
+  if (tldr.style.display === "none") {
 
-      tldr.style.opacity = "0";
-      tldr.style.height = "0";
-      fullText.style.opacity = "0";
-      fullText.style.height = "0";
-      button.innerHTML = "Mutass többet";
+    tldr.style.opacity = "0";
+    tldr.style.height = "0";
+    fullText.style.opacity = "0";
+    fullText.style.height = "0";
+    button.innerHTML = "Mutass többet";
+    setTimeout(function() {
+      tldr.style.display = "block";
+      fullText.style.display = "none";
       setTimeout(function() {
-        tldr.style.display = "block";
-        fullText.style.display = "none";
-        setTimeout(function() {
-          tldr.style.opacity = "1";
-          tldr.style.height = tldr.scrollHeight + "px";
-        }, 0);
-      }, 500);
-    } else {
-
-      fullText.style.opacity = "0";
-      fullText.style.height = "0";
-      tldr.style.opacity = "0";
-      tldr.style.height = "0";
-      button.innerHTML = "Kevesebb";
-      setTimeout(function() {
-        fullText.style.display = "block";
-        tldr.style.display = "none";
-        setTimeout(function() {
-          fullText.style.opacity = "1";
-          fullText.style.height = fullText.scrollHeight + "px";
-        }, 0);
+        tldr.style.opacity = "1";
+        tldr.style.height = tldr.scrollHeight + "px";
       }, 0);
-    }
-  }
+    }, 500);
+  } else {
 
+    fullText.style.opacity = "0";
+    fullText.style.height = "0";
+    tldr.style.opacity = "0";
+    tldr.style.height = "0";
+    button.innerHTML = "Kevesebb";
+    setTimeout(function() {
+      fullText.style.display = "block";
+      tldr.style.display = "none";
+      setTimeout(function() {
+        fullText.style.opacity = "1";
+        fullText.style.height = fullText.scrollHeight + "px";
+      }, 0);
+    }, 0);
+  }
+}
 
 function szelesseg(){
   var element1 = document.getElementById('csapos_eman');
