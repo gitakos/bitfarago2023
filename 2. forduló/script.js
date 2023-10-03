f9df070867c74e0d229f56c75fa4ee8ccc451e6f
Kovács Ákos <kovacs.akos@ckik.loc>
2023-10-03 10:34:32 +0200

    nicsn kesz

--- 2. forduló/script.js
+++ 2. forduló/script.js
@@ -1,11 +1,12 @@
- 
+ const babuTemplate = ["bastya","lo","futo","kiraly","kiralyno","futo","lo","bastya","paraszt","paraszt","paraszt","paraszt","paraszt","paraszt","paraszt","paraszt"]
+ var tablaHatterTar = [];
  function tablaGen(){
     var tabla = document.getElementById("tablaTartalom");
     var mezo = 0;
     for (var i = 0; i < 12; i++) {
         var sor = document.createElement("tr");
-   
         for (var j = 0; j < 8; j++) {
+            tablaHatterTar.push(0);
             var cella = document.createElement("td");
             cella.id = mezo;
             cella.textContent = mezo;
@@ -16,5 +17,23 @@
         tabla.appendChild(sor);
     }
  }
-
+function babuGen(){
+    for (let index = 0; index < babuTemplate.length; index++) {
+        tablaHatterTar[index] = babuTemplate[index];
+    }
+    console.log(babuTemplate);
+    for (let index = tablaHatterTar.length-1; index > tablaHatterTar.length-babuTemplate.length-1; index--) {
+        console.log(index);
+        tablaHatterTar[index] = babuTemplate[index-(tablaHatterTar.length-babuTemplate.length+index)];
+    }
+}
+function kimutat(){
+    var tabla = document.getElementById("tablaTartalom");
+    var cellak = tabla.getElementsByTagName("td");
+    for (var i = 0; i < cellak.length; i++) {
+        cellak[i].textContent = tablaHatterTar[i];
+    }
+}
  tablaGen();
+ babuGen();
+ kimutat();
